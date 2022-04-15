64bba2ad4d40e5fdd41542974c533938346d0461
XenoLord <103731872+XenoLord@users.noreply.github.com>
2022-04-15 13:56:56 +0300

    Update .lua

--- .lua
+++ .lua
@@ -9,3 +9,5 @@ loadstring(game:HttpGet("https://pastebin.com/raw/Wp3B2QfJ"))()
 NOTE: Dont forget to delete your bank members and cancel all outgoing invites
 NOTE2: It only works at tier2 - tier8 banks.
 
+Made by XenoLord - emoemoemoemoemoemo23
+  
